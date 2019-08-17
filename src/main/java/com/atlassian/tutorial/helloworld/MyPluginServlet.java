072002175a6a3506643db9497e5bb398ad939dd6
Gelena Kozmina <gelena.kozmina@gmail.com>
2019-08-16 19:25:01 -0700

    Add servlet

---
+++ src/main/java/com/atlassian/tutorial/helloworld/MyPluginServlet.java
@@ -0,0 +1,21 @@
+package com.atlassian.tutorial.helloworld;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyPluginServlet extends HttpServlet {
+	private static final Logger log = LoggerFactory.getLogger(MyPluginServlet.class);
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		resp.getWriter().write("<html><body>Hello! You did it.</body></html>");
+
+	}
+}
